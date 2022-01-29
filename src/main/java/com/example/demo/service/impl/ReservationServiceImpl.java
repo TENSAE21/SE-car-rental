a7b8a34ea50772f0591ac75cd6a46742197c7961
EliltaW <eliltawondimu12@gmail.com>
2022-01-29 02:11:18 -0600

    Lombok&DevTools, Annotations, ServiceImpl, ValueObject interface

---
+++ src/main/java/com/example/demo/service/impl/ReservationServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.service.ReservationService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReservationServiceImpl implements ReservationService {
+}
